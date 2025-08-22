cbc0467ad6bcf93a95b1bd6e9549514804088deb
nikolafirst <135094267+nikolafirst@users.noreply.github.com>
2025-08-23 01:20:20 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * My test task
+ * @author Nikolay Karasev
+ * @version 0.1
+ */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String result = calc(input);
+        System.out.print(result);
+    }
+
+    static String calc(String input) {
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            return "throws Exception";
+        }
+
+        try {
+            int num1 = Integer.parseInt(parts[0]);
+            String operator = parts[1];
+            int num2 = Integer.parseInt(parts[2]);
+
+            switch (operator) {
+                case "+":
+                    return String.valueOf(num1 + num2);
+                case "-":
+                    return String.valueOf(num1 - num2);
+                case "*":
+                    return String.valueOf(num1 * num2);
+                case "/":
+                    if (num2 == 0) {
+                        return "Ошибка: деление на ноль.";
+                    }
+                    return String.valueOf(num1 / num2);
+                default:
+                    return "throws Exception";
+            }
+        } catch (NumberFormatException e) {
+            return "throws Exception";
+        }
+    }
+}
